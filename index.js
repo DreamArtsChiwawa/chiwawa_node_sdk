cc8d2826e590ba6ddc9bcafeaff194c50180e8d0
Akira-Isegawa <akira.isegawa@gmail.com>
2017-09-06 17:34:28 +0900

    インターフェースを汎用化

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 /** 知話輪関連の処理 */
-var request = require('request');
+const request = require('request');
+
 const ChiwawaService = {
     /** リクエストの内容をチェック。
      * @param req リクエストオブジェクト
-     * @param context（任意） Azure functionsの場合は、contextをセットすると、問題があった際にエラーレスポンスを返し、contextを処理済みにする。
+     * @param azureContext Azure functionsの場合は、contextをセットすると、問題があった際にエラーレスポンスを返し、contextを処理済みにする。
      * @return 問題がある場合はfalseを返す。。 
      */
-    isValidRequest: function(req, context) {
+    isValidRequest: function(req, azureContext) {
         // headerをチェック
         if (!ChiwawaService.privateMethods.isAuthorized(req)) {
-            if (context) {
-                context.res = {
+            if (azureContext) {
+                azureContext.res = {
                     status: 401,
                     body: "Unauthorized request."
                 };
-                context.log("401: Unauthorized request.");
-                context.done();
+                azureContext.log("401: Unauthorized request.");
+                azureContext.done();
             }
             return false;
         }
         
         // bodyをチェック
         if (!ChiwawaService.privateMethods.isBodyValid(req)) {
-            if (context) {
-                context.res = {
+            if (azureContext) {
+                azureContext.res = {
                     status: 400,
                     body: "Request body is not valid. Please set a message in the request body."
                 };
-                context.log("400: Bad request.");
-                context.done();
+                azureContext.log("400: Bad request.");
+                azureContext.done();
             }
             return false;
         }
@@ -36,41 +37,102 @@ const ChiwawaService = {
     },
     /** リクエストから企業IDを取得 */
     getCompanyId: function(req) {
-        return req.body.companyId;
+        if (req && req.body) {
+            return req.body.companyId || "";
+        } else {
+            return "";
+        }
     },
     /** リクエストからグループIDを取得 */
     getGroupId: function(req) {
-        return req.body.message.groupId;
-    },
-    /** リクエストからメッセージを取得 */
-    getMessage: function(req) {
-        return req.body.message.text;
-    },
-    /** 知話輪にメッセージを送信 */
-    sendMessage: function(companyId, groupId, messageText, callback) {
-        const baseUrl = `https://${companyId}.chiwawa.one/api/public/v1/groups/${groupId}/messages`;
-        const content = {
-            'text': messageText
-        };
-        const headers = {
-            'Content-Type': 'application/json',
-            'X-Chiwawa-API-Token': process.env["CHIWAWA_API_TOKEN"]
-        };
-        const options = {
-            url: baseUrl,
-            headers: headers,
-            json: content
+        if (req && req.body && req.body.message) {
+            return req.body.message.groupId || "";
+        } else {
+            return "";
         }
-        request.post(options, function(err, httpResponse, body){
-            if (callback) callback(err, httpResponse, body);
-        });
+    },
+    /** リクエストからメッセージの本文を取得 */
+    getMessageText: function(req) {
+        if (req && req.body && req.body.message) {
+            return req.body.message.text || "";
+        } else {
+            return "";
+        }
+    },
+    /** 知話輪にテキストメッセージを送信 */
+    sendMessage: function(req, messageText, callback, azureContext) {
+        const content = ChiwawaService.MessageDataBuilder.newMessageDataWithText(messageText);
+        ChiwawaService.privateMethods.sendMessage(ChiwawaService.getCompanyId(req), ChiwawaService.getGroupId(req), content, callback, azureContext);
+    },            
+    /** 知話輪に指定したメッセージデータをそのまま送信（複雑なメッセージデータを送る場合） */
+    sendMessageRawData: function(req, messageData, callback, azureContext) {
+        ChiwawaService.privateMethods.sendMessage(ChiwawaService.getCompanyId(req), ChiwawaService.getGroupId(req), messageData, callback, azureContext);
+    },
+    MessageDataBuilder: {
+        newMessageDataWithText: function(messageText) {
+            return {
+                text: messageText || "",
+            }
+        },
+        newMessageDataWithTextAttachment: function(messageText, attachmentTitle, attachmentText, textType) {
+            const textAttachment = ChiwawaService.MessageDataBuilder.newTextAttachment(attachmentTitle, attachmentText, textType);
+            return {
+                text: messageText || "",
+                attachments: [textAttachment]
+            };
+        },
+        newMessageDataWithAttachments: function(messageText, attachments) {
+            return {
+                text: messageText || "",
+                attachments: attachments
+            };
+        },
+        newTextAttachment: function(attachmentTitle, attachmentText, textType, color, tagIcons, displaysCommentField, actions) {
+            return {
+                viewType: "text",
+                title: attachmentTitle || "",
+                text: attachmentText || "",
+                textType: textType || "none",
+                color: color || "",
+                tagIcons: tagIcons || [],
+                displaysCommentField: displaysCommentField || "no",
+                actions: actions || []
+            };
+        }
     },
     privateMethods: {
         isAuthorized: function(req) {
-            return req.headers['x-chiwawa-webhook-token'] === process.env["CHIWAWA_VALIDATION_TOKEN"];
+            if (req && req.headers) {
+                return req.headers['x-chiwawa-webhook-token'] === process.env["CHIWAWA_VALIDATION_TOKEN"];                
+            } else {
+                return false;
+            }
         },
         isBodyValid: function(req) {
             return req.body && req.body.type && req.body.message;
+        },
+        /** 知話輪にメッセージを送信 */
+        sendMessage: function(companyId, groupId, content, callback, azureContext) {
+            const baseUrl = `https://${companyId}.chiwawa.one/api/public/v1/groups/${groupId}/messages`;
+            const headers = {
+                'Content-Type': 'application/json',
+                'X-Chiwawa-API-Token': process.env["CHIWAWA_API_TOKEN"]
+            };
+            const options = {
+                url: baseUrl,
+                headers: headers,
+                json: content
+            }
+            request.post(options, function(err, httpResponse, body){
+                if (callback) callback(err, httpResponse, body);
+            });
+            if (azureContext) {
+                azureContext.res = {
+                    status: 200,
+                    body: "OK."
+                };
+                azureContext.done();    
+            }
         },
     }
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,142 @@
+var assert = require("assert")
 const ChiwawaService = require('../index');
 
-var req = {
-  body: {
-    companyId: "abc"
-  }
-};
+const VALIDATION_TOKEN = "TEST_TOKEN";
+const API_TOKEN = "YOUR_API_TOKEN";
+const TEST_COMPANY_ID = "YOURC|_COMPANY_ID";
+const TEST_GROUP_ID = "YOUR_GROUP_ID";
 
-console.log(ChiwawaService.getCompanyId(req));
+process.env["CHIWAWA_VALIDATION_TOKEN"] = VALIDATION_TOKEN;
+process.env["CHIWAWA_API_TOKEN"] = API_TOKEN;
+
+function getRequest() {
+  return {
+    headers: {'x-chiwawa-webhook-token': VALIDATION_TOKEN},
+    body: {
+      companyId: TEST_COMPANY_ID,
+      type: "message",
+      message: {
+        groupId: TEST_GROUP_ID,
+        text: "TEST_MESSAGE"
+      }
+    }
+  };
+}
+
+describe('ChiwawaServiceのテスト', function() {
+  describe('isValidRequest', function() {
+    it('正しくトークン、bodyが設定されている場合', function() {
+      assert.equal(ChiwawaService.isValidRequest(getRequest()), true);
+    });  
+    it('トークンが不正、bodyが正しく設定されている場合', function() {
+      let newReq = getRequest();
+      newReq.headers = null;
+      assert.equal(ChiwawaService.isValidRequest(newReq), false);
+    });  
+    it('トークンが正しく、bodyが不正の場合', function() {
+      let newReq = getRequest();
+      newReq.body = null;
+      assert.equal(ChiwawaService.isValidRequest(newReq), false);
+    });  
+    it('トークン、bodyともに不正の場合', function() {
+      let newReq = getRequest();
+      assert.equal(ChiwawaService.isValidRequest(getRequest()), true);
+      newReq.headers = null;
+      newReq.body = null;
+      assert.equal(ChiwawaService.isValidRequest(newReq), false);
+    });  
+  });
+
+  describe('getCompanyId', function() {
+    it('正常系', function() {
+      assert.equal(ChiwawaService.getCompanyId(getRequest()), TEST_COMPANY_ID);
+    });
+    it('companyIdがnull', function() {
+      let newReq = getRequest();
+      newReq.body.companyId = null;
+      assert.equal(ChiwawaService.getCompanyId(newReq), "");
+    });
+    it('bodyがnull', function() {
+      let newReq = getRequest();
+      newReq.body = null;
+      assert.equal(ChiwawaService.getCompanyId(newReq), "");
+    });  
+    it('requestがnull', function() {
+      assert.equal(ChiwawaService.getCompanyId(null), "");
+    });    
+  });
+  describe('getGroupId', function() {
+    it('正常系', function() {
+      assert.equal(ChiwawaService.getGroupId(getRequest()), TEST_GROUP_ID);
+    });
+    it('groupIdがnull', function() {
+      let newReq = getRequest();
+      newReq.body.message.groupId = null;
+      assert.equal(ChiwawaService.getGroupId(newReq), "");
+    });
+    it('messageがnull', function() {
+      let newReq = getRequest();
+      newReq.body.message = null;
+      assert.equal(ChiwawaService.getGroupId(newReq), "");
+    });  
+    it('bodyがnull', function() {
+      let newReq = getRequest();
+      newReq.body = null;
+      assert.equal(ChiwawaService.getGroupId(newReq), "");
+    });  
+    it('requestがnull', function() {
+      assert.equal(ChiwawaService.getGroupId(null), "");
+    });    
+  });
+  describe('getMessageText', function() {
+    it('正常系', function() {
+      assert.equal(ChiwawaService.getMessageText(getRequest()), "TEST_MESSAGE");
+    });
+    it('textがnull', function() {
+      let newReq = getRequest();
+      newReq.body.message.text = null;
+      assert.equal(ChiwawaService.getMessageText(newReq), "");
+    });
+    it('messageがnull', function() {
+      let newReq = getRequest();
+      newReq.body.message = null;
+      assert.equal(ChiwawaService.getMessageText(newReq), "");
+    });  
+    it('bodyがnull', function() {
+      let newReq = getRequest();
+      newReq.body = null;
+      assert.equal(ChiwawaService.getMessageText(newReq), "");
+    });  
+    it('requestがnull', function() {
+      assert.equal(ChiwawaService.getMessageText(null), "");
+    });    
+  });
+
+  describe('sendMessage', function() {
+    it('正常系', function(done) {
+      ChiwawaService.sendMessage(getRequest(), "SDK単体テスト　sendMessage", function(err, httpResponse, body) {
+        if (err) {
+          assert.fail(err);
+        } else {
+          console.log("OK");
+        }
+        done();
+      });
+    });
+  });
+  describe('sendMessageRawData', function() {
+    it('正常系', function(done) {
+      const content = ChiwawaService.MessageDataBuilder.newMessageDataWithText("SDK単体テスト　sendMessageRawData");
+      ChiwawaService.sendMessageRawData(getRequest(), content, function(err, httpResponse, body) {
+        if (err) {
+          assert.fail(err);
+        } else {
+          console.log("OK");
+        }
+        done();
+      });
+    });
+  });
+
+});
+
